043ab19610fb22bfcc616b9ecb279a20ca53f546
Eric Bernard <eric99b@gmail.com>
2020-08-15 17:51:52 -0400

    Updates

---
+++ precache-manifest.7fda8ca2a7b476c267dc18bdfdd1f336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef00ae5969ea5fde55626ebc63710e8b",
+    "url": "/ebernerd.com/index.html"
+  },
+  {
+    "revision": "ac1570390130734e3c0c",
+    "url": "/ebernerd.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "104c03381f99012bc865",
+    "url": "/ebernerd.com/static/js/2.73f93f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ebernerd.com/static/js/2.73f93f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1570390130734e3c0c",
+    "url": "/ebernerd.com/static/js/main.c26cebea.chunk.js"
+  },
+  {
+    "revision": "421b5bbfe7ae56d30af6",
+    "url": "/ebernerd.com/static/js/runtime-main.87e0ca45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ebernerd.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
